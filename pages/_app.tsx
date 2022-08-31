1e91aba4f658654472a6777a8e4d6b7179b8e500
Mad Max <madmaxmehdi44@gmail.com>
2022-08-31 17:29:31 +0430

    hhgjghjghj

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,9 @@
-import {ErrorFallbackProps, ErrorComponent, ErrorBoundary, AppProps} from "@blitzjs/next"
-import {AuthenticationError, AuthorizationError} from "blitz"
+import { ErrorFallbackProps, ErrorComponent, ErrorBoundary, AppProps } from "@blitzjs/next"
+import { AuthenticationError, AuthorizationError } from "blitz"
 import React from "react"
-import {withBlitz} from "app/blitz-client"
+import { withBlitz } from "app/blitz-client"
 
-function RootErrorFallback({error}: ErrorFallbackProps) {
+function RootErrorFallback({ error }: ErrorFallbackProps) {
   if (error instanceof AuthenticationError) {
     return <div>Error: You are not authenticated</div>
   } else if (error instanceof AuthorizationError) {
@@ -17,13 +17,13 @@ function RootErrorFallback({error}: ErrorFallbackProps) {
     return (
       <ErrorComponent
         statusCode={(error as any)?.statusCode || 400}
-        title={error.message || error.name}
+        title={"error.message" || error.name}
       />
     )
   }
 }
 
-function MyApp({Component, pageProps}: AppProps) {
+function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ErrorBoundary FallbackComponent={RootErrorFallback}>
       <Component {...pageProps} />
