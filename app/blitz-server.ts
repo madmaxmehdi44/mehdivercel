c1fdf1d81960ff1abb68d8acf66750fccbaa0a4c
Mad Max <madmaxmehdi44@gmail.com>
2022-08-31 16:04:45 +0430

    msmsm

--- app/blitz-server.ts
+++ app/blitz-server.ts
@@ -1,9 +1,9 @@
 import { setupBlitzServer } from "@blitzjs/next"
 import { AuthServerPlugin, PrismaStorage } from "@blitzjs/auth"
 import { simpleRolesIsAuthorized } from "@blitzjs/auth"
-import db from "db"
+import db, { UserRole } from "db"
 import { authConfig } from "./blitz-client"
-
+export type Role = UserRole
 export const { gSSP, gSP, api } = setupBlitzServer({
   plugins: [
     AuthServerPlugin({
@@ -13,4 +13,3 @@ export const { gSSP, gSP, api } = setupBlitzServer({
     }),
   ],
 })
-
--- types.ts
+++ types.ts
@@ -1,9 +1,12 @@
-import { SimpleRolesIsAuthorized } from "@blitzjs/auth"
-import { User } from "db"
+import { SessionContext, SimpleRolesIsAuthorized, BlitzCtx } from "@blitzjs/auth"
+import { User, UserRole } from "db"
 
-export type Role = "ADMIN" | "USER"
+export type Role = UserRole
 
 declare module "@blitzjs/auth" {
+  export interface Ctx extends BlitzCtx {
+    session: SessionContext
+  }
   export interface Session {
     isAuthorized: SimpleRolesIsAuthorized<Role>
     PublicData: {
